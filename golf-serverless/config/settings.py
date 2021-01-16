fbd2a6fbb8cfec5e0e0b8c16cf1817d7c23368b4
Steve Russo <64294847+sjrusso8@users.noreply.github.com>
2021-01-16 15:03:12 -0500

    fixed issue with simple JWT missed spelled the config settings as SIMPLE_JET...

--- golf-serverless/config/settings.py
+++ golf-serverless/config/settings.py
@@ -241,7 +241,7 @@ REST_FRAMEWORK = {
 
 # JWT AUTH SETTINGS
 # ------------------------------------------------------------------------------
-SIMPLE_JET = {
+SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
